60e41f74c5415b7a5a6fd496719827b337da27dd
maxiy01 <maxiy01@mail.ru>
2021-11-13 18:34:07 +0400

    Added test alerts

--- ardenti-silva-frontend/src/App.js
+++ ardenti-silva-frontend/src/App.js
@@ -8,21 +8,20 @@ import {Alert} from "./AlertComponent"
 
 const SIDEBAR_WIDTH = 288;
 
+
 function App() {
   return (
     <div className="App">
       <CameraTable cameras={camera_urls} cells_width={5} video_width={320} video_height={240}></CameraTable>
       <Sidebar
         sidebar={<div>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
-                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'1. Бай-Тал'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'8. 	Хемчик'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'14. Барлык'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'7. 	Шекпээр'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'11. Кирсарай'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'22. Шеми'}/>
+                <Alert image="https://i.imgur.com/N2rlKZp.jpeg" width={SIDEBAR_WIDTH} camera_id={'13. Чадан'}/>
           </div>}
         open={true}
         styles={{ sidebar: { background: "white", overflowX: "hidden", overflowY: 'scroll'}}}
