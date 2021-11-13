397936c890889f99314810fceb66df6638145583
Max Zykin <sanchez.max@yandex.ru>
2021-11-13 17:57:47 +0400

    add sidebar

--- ardenti-silva-frontend/src/components/camera/index.js
+++ ardenti-silva-frontend/src/components/camera/index.js
@@ -66,6 +66,25 @@ const TasksTable = () => {
     return (
         <div style={{ "margin": "50px", "padding": "50px" }}>
             <CameraTable cameras={camera_urls} cells_width={4} video_width={320} video_height={240}></CameraTable>
+                        {/* <Sidebar
+        sidebar={<div>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+                <Alert image="https://avatars.mds.yandex.net/get-zen_doc/3510533/pub_606477fbf57165397e606cc8_606478be12fb21100c1084eb/scale_1200" width={SIDEBAR_WIDTH}/>
+          </div>}
+        open={true}
+        styles={{ sidebar: { background: "white", overflowX: "hidden", overflowY: 'scroll'}}}
+        docked={true}
+        pullRight={true}
+        sidebarClassName="alerts"
+      >
+      </Sidebar> */}
         </div>
     )
 }
