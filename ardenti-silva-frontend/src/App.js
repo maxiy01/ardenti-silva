013e2d8e1da9a5a26b0123b395843d9d2ab98d0c
maxiy01 <maxiy01@mail.ru>
2021-11-13 14:22:31 +0400

    Moved camera urls to config.json

--- ardenti-silva-frontend/src/App.js
+++ ardenti-silva-frontend/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {CameraTable} from './CameraTableComponent'
-
-let free_cameras_url = [
-  "http://61.115.123.234/-wvhttp-01-/getoneshot?camera_id=1&v=320&frame_count=15",
-  "http://laura.paide.ee:8888/axis-cgi/mjpg/video.cgi?camera=&resolution=320x240&fps=15",
-  "http://webcam01.ecn.purdue.edu/axis-cgi/mjpg/video.cgi",
-  "http://camera3.buffalotrace.com/axis-cgi/mjpg/video.cgi",
-  "http://74.92.201.146/mjpg/video.mjpg",
-  "http://klosterplatz.selfip.info/mjpg/video.mjpg",
-  "http://83.146.208.254:81/cgi-bin/faststream.jpg?stream=full&fps=15",
-  "http://csf4utnf.dyndns.org:8080/cgi-bin/faststream.jpg?stream=full&fps=15",
-  "http://camera.kkbtv.com:8080/nphMotionJpeg?resolution=320x240",
-  "http://62.108.201.7/cgi-bin/faststream.jpg?stream=full&fps=24"
-]
+import {CameraTable} from './CameraTableComponent';
+import {camera_urls} from './config.json'
 
 function App() {
   return (
     <div className="App">
-      <CameraTable cameras={free_cameras_url} cells_width={5} video_width={320} video_height={240}></CameraTable>
+      <CameraTable cameras={camera_urls} cells_width={5} video_width={320} video_height={240}></CameraTable>
     </div>
   );
 }
